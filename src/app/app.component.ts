3e7d537935450918c37f47f2720294f0f10a6d77
vvk30 <vishal.kulkarni303@gmail.com>
2017-03-22 17:44:58 +0530

    First Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component,Pipe } from '@angular/core';
+import {AngularFire, AuthProviders, AuthMethods, FirebaseListObservable} from 'angularfire2';
+import "rxjs/add/operator/map";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app works!';
-}
+  items: any
+  name:any
+  msgVal:string = ''
+  constructor(private af: AngularFire){
+    this.items = af.database.list('/messages',{
+      query: {
+        limitToLast: 7
+      }
+    }).map((array)=> array.reverse())
+    this.af.auth.subscribe(auth=>{
+      if(auth){
+        this.name = auth
+      }
+    })
+  }
+  loginGl(){
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    })
+  }
+  chatSend(theirMessage: string){
+    this.items.push({
+      message: theirMessage, name: this.name.google.displayName});
+      this.msgVal = '';
+    }
+    logout() {
+     this.af.auth.logout();
+     location.reload();
+  }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCnk17OWjytDF0gC5T5ADir_30pisCyqWQ",
+    authDomain: "mychatapp-c01f6.firebaseapp.com",
+    databaseURL: "https://mychatapp-c01f6.firebaseio.com",
+    storageBucket: "mychatapp-c01f6.appspot.com",
+    messagingSenderId: "524991244257"
+}
 
 @NgModule({
   declarations: [
@@ -12,9 +20,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
